37c6e58ad8b6b1ff1b1b642264ed5cfa6ea4f446
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-02-19 20:52:14 -0300

    up

---
+++ static/script.js
@@ -0,0 +1,13 @@
+
+
+function Media(){
+    var n1=window.document.getElementById("txtn1");
+    var n2=window.document.getElementById("txtn2");
+    var valor1=Number.parseInt(n1.Vale);
+    var valor2=Number.parseInt(n2);
+    var Res= valor1/valor2;
+    
+   
+    
+
+}
